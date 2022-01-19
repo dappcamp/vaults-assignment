df50b725124f3cbfdf7d0b6c62b71ba5e97bbc1b
Jungho Bang <jungho.bang@coinbase.com>
2022-01-19 11:30:27 -0800

    scaffolding

--- contracts/Vault1.sol
+++ contracts/Vault1.sol
@@ -1,6 +1,12 @@
 //SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.4;
 
-contract Vault1 {
-    
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract Vault1 is ERC20 {
+    constructor() ERC20("VAULT", "VAULT") {}
+
+    function deposit(uint256 _amount) external payable {}
+
+    function withdraw(uint256 _amount) external {}
 }
--- contracts/Vault2.sol
+++ contracts/Vault2.sol
@@ -1,6 +1,18 @@
 //SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.4;
 
-contract Vault2 {
-    
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract Vault2 is ERC20, ERC20Burnable, Ownable {
+    constructor() ERC20("VAULT", "VAULT") {}
+
+    function mint(address to, uint256 amount) external onlyOwner {
+        _mint(to, amount);
+    }
+
+    function burn(uint256 amount) public override onlyOwner {
+        super.burn(amount);
+    }
 }
