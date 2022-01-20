87aa92b82f4c13244d9b818999779be644969df2
Todd Li <forcey@gmail.com>
2022-01-20 07:59:11 -0800

    Vault1

--- contracts/Vault1.sol
+++ contracts/Vault1.sol
@@ -1,6 +1,18 @@
 //SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.4;
 
-contract Vault1 {
-    
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Wrapper.sol";
+
+contract Vault1 is ERC20Wrapper {
+    constructor(IERC20 underlyingToken) 
+        ERC20Wrapper(underlyingToken) 
+        ERC20("Vault1", "VALT1") {}
+
+    function deposit(uint _amount) public {
+        depositFor(msg.sender, _amount);
+    }
+
+    function withdraw(uint _amount) public {
+        withdrawTo(msg.sender, _amount);
+    }
 }
--- test/Vault1Test.js
+++ test/Vault1Test.js
@@ -4,10 +4,33 @@ const { ethers } = require("hardhat");
 describe("Vault 1", () => {
 	beforeEach(async () => {
 		Vault1 = await ethers.getContractFactory("Vault1");
-		[owner] = await ethers.getSigners();
+		const accounts = await ethers.getSigners();
+
+		owner = accounts[0];
+		account1 = accounts[1];
 
 		vault1 = await Vault1.deploy();
 
 		await vault1.deployed();
+	});
+
+	describe("deposit", function () {
+		it("should let anyone deposit token", async function () {
+			await expect(
+				vault1.connect(account1).add(AnimalType.Fish, 5)
+			).to.be.revertedWith("Not an owner");
+		});
+
+		it("should revert when invalid animal is provided", async function () {
+			await expect(
+				vault1.connect(owner).add(AnimalType.None, 5)
+			).to.be.revertedWith("Invalid animal");
+		});
+
+		it("should emit added event when pet is added", async function () {
+			await expect(vault1.connect(owner).add(AnimalType.Fish, 5))
+				.to.emit(vault1, "Added")
+				.withArgs(AnimalType.Fish, 5);
+		});
 	});
 });
