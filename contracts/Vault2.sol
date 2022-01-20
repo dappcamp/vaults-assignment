72bc28ab5c14ae825bb93b30a382ffdea138f160
Todd Li <forcey@gmail.com>
2022-01-20 08:47:35 -0800

    Vault2

--- contracts/Vault2.sol
+++ contracts/Vault2.sol
@@ -1,6 +1,26 @@
 //SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.4;
 
-contract Vault2 {
-    
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract Vault2 is ERC20 {
+    event Minted(address owner, uint amount);
+    event Burned(address owner, uint amount);
+
+    constructor() 
+        ERC20("Vault2", "VALT2") {}
+
+    function mint(uint _amount) public payable {
+        require(_amount != 0, "Must mint a non-zero value.");
+        require(msg.value == _amount, "Must send exactly the same amount of ether.");
+        _mint(msg.sender, _amount);
+        emit Minted(msg.sender, _amount);
+    }
+
+    function burn(uint _amount) public {
+        require(_amount != 0, "Must burn a non-zero value.");
+        _burn(msg.sender, _amount);
+        payable(msg.sender).transfer(_amount);
+        emit Burned(msg.sender, _amount);
+    }
 }
--- test/Vault2Test.js
+++ test/Vault2Test.js
@@ -10,4 +10,52 @@ describe("Vault 2", () => {
 
 		await vault2.deployed();
 	});
+
+	describe("mint", function() {
+		it("should mint with exactly amount of ether sent", async function () {
+			await expect(
+				vault2.mint(1000, { value: 1000 })
+			).to.emit(vault2, "Minted").withArgs(owner.address, 1000);
+		});
+
+		it("should not mint with more ether sent", async function () {
+			await expect(
+				vault2.mint(1000, { value: 1001 })
+			).to.be.revertedWith("Must send exactly the same amount of ether.");
+		});
+
+		it("should not mint with less ether sent", async function () {
+			await expect(
+				vault2.mint(1000, { value: 999 })
+			).to.be.revertedWith("Must send exactly the same amount of ether.");
+		});
+
+		it("should not mint zero", async function () {
+			await expect(
+				vault2.mint(0)
+			).to.be.revertedWith("Must mint a non-zero value.");
+		});
+	});
+
+	describe("burn", function() {
+		it("should burn if balance is sufficient", async function () {
+			await vault2.mint(1000, { value: 1000 });
+			await expect(
+				vault2.burn(500)
+			).to.emit(vault2, "Burned").withArgs(owner.address, 500);
+		});
+
+		it("should not burn if balance is insufficient", async function () {
+			await vault2.mint(1000, { value: 1000 });
+			await expect(
+				vault2.burn(1001)
+			).to.be.revertedWith("ERC20: burn amount exceeds balance");
+		});
+
+		it("should not burn zero", async function () {
+			await expect(
+				vault2.burn(0)
+			).to.be.revertedWith("Must burn a non-zero value.");
+		});
+	});
 });
