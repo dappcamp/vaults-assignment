85104a330b107cabe7184e496c3989a7f09e2f15
rcsheng@gmail.com <rcsheng@example.com>
2022-01-20 09:05:39 -0500

    2022.1.20 version

--- contracts/Vault1.sol
+++ contracts/Vault1.sol
@@ -1,6 +1,32 @@
 //SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.4;
 
-contract Vault1 {
-    
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+// Specifications
+// function deposit(_amount) - Should take in deposit amount. Assume that the contract is pre-approved to transfer that amount
+// function withdraw(_amount) - Should allow users to withdraw amount lesser than or equal to what they have deposited
+contract Vault1 is ERC20 {
+
+    mapping(address => uint256) private _balances;
+
+    constructor() ERC20("Vault", "vault") public {
+        // super._mint(msg.sender,100*10**18);
+     }
+
+    function deposit(uint256 _amount) external payable returns(bool) {
+        // _balances[msg.sender] += _amount;
+        super.approve(msg.sender, _amount);
+        return super.transferFrom(msg.sender,address(this), _amount);
+    }
+
+    function withdraw(uint256 _amount) external returns(bool) {
+        require(_amount <= super.balanceOf(msg.sender), "Cannot withdraw more than balance");
+        // _balances[msg.sender] -= _amount;
+        return super.transferFrom(address(this),msg.sender, _amount);
+    }
+
+    // function balanceOf(address _account) public view virtual override returns (uint256) {
+    //     return _balances[_account];
+    // }
 }
--- test/Vault1Test.js
+++ test/Vault1Test.js
@@ -4,10 +4,21 @@ const { ethers } = require("hardhat");
 describe("Vault 1", () => {
 	beforeEach(async () => {
 		Vault1 = await ethers.getContractFactory("Vault1");
-		[owner] = await ethers.getSigners();
+		[owner, recipient, other] = await ethers.getSigners();
 
 		vault1 = await Vault1.deploy();
 
 		await vault1.deployed();
+	});
+
+	describe("deposit", function () {
+		it("Should take in deposit amount", async function () {
+			await vault1.connect(owner).deposit(500);
+			const ownerBalance = await vault1.balanceOf(owner.address);
+			expect(await vault1.totalSupply()).to.equal(ownerBalance);
+			
+		});
+
+		
 	});
 });
