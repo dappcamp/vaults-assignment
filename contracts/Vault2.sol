9b048b68e85a3b063858d271aa951cc325a3b62a
Róbert Istók <robertistok94@gmail.com>
2022-01-20 18:15:03 +0200

    Implement tasks

--- contracts/Vault2.sol
+++ contracts/Vault2.sol
@@ -1,6 +1,22 @@
 //SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.4;
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-contract Vault2 {
-    
+contract Vault2 is ERC20 {
+    event Deposit(address indexed _from, uint256 amount);
+    event Withdraw(address indexed _to, uint256 amount);
+
+    constructor() ERC20("VAULT", "VLT") {}
+
+    function mint() public payable {
+        _mint(msg.sender, msg.value);
+        emit Deposit(msg.sender, msg.value);
+    }
+
+    function burn(uint256 _amount) public {
+        require(balanceOf(msg.sender) >= _amount);
+        _burn(msg.sender, _amount);
+        payable(msg.sender).transfer(_amount);
+        emit Withdraw(msg.sender, _amount);
+    }
 }
--- test/Vault1Test.js
+++ test/Vault1Test.js
@@ -2,12 +2,113 @@ const { expect } = require("chai");
 const { ethers } = require("hardhat");
 
 describe("Vault 1", () => {
-	beforeEach(async () => {
-		Vault1 = await ethers.getContractFactory("Vault1");
-		[owner] = await ethers.getSigners();
+  let vault1, token1, token2;
 
-		vault1 = await Vault1.deploy();
+  beforeEach(async () => {
+    Vault1 = await ethers.getContractFactory("Vault1");
+    const Token1 = await ethers.getContractFactory("Token1");
+    const Token2 = await ethers.getContractFactory("Token2");
 
-		await vault1.deployed();
-	});
+    [owner, addr1] = await ethers.getSigners();
+
+    vault1 = await Vault1.deploy();
+    token1 = await Token1.deploy();
+    token2 = await Token2.deploy();
+
+    await vault1.deployed();
+    await token1.deployed();
+    await token2.deployed();
+  });
+
+  it("should deposit ERC20 into vault", async () => {
+    const initialToken1Amount = await token1.balanceOf(owner.address);
+    const initialToken2Amount = await token2.balanceOf(owner.address);
+
+    expect(await vault1.tokenBalanceOf(owner.address, token1.address)).to.equal(
+      0
+    );
+
+    // deposits token1 into vault
+    await token1.connect(owner).approve(vault1.address, 1000);
+    await vault1.connect(owner).deposit(100, token1.address);
+    expect(await vault1.tokenBalanceOf(owner.address, token1.address)).to.equal(
+      100
+    );
+    expect(await token1.balanceOf(owner.address)).to.equal(
+      initialToken1Amount - 100
+    );
+
+    await token2.connect(owner).approve(vault1.address, 1000);
+    // deposits token2 into vault
+    await vault1.connect(owner).deposit(200, token2.address);
+    expect(await vault1.tokenBalanceOf(owner.address, token2.address)).to.equal(
+      200
+    );
+    expect(await token2.balanceOf(owner.address)).to.equal(
+      initialToken2Amount - 200
+    );
+  });
+
+  it("should withdraw ERC20 into vault", async () => {
+    // deposits token1 into vault
+    await token1.connect(owner).approve(vault1.address, 1000);
+    await vault1.connect(owner).deposit(100, token1.address);
+    const token1AmountOfOwner = await token1.balanceOf(owner.address);
+    await vault1.connect(owner).withdraw(50, token1.address);
+    expect(await vault1.tokenBalanceOf(owner.address, token1.address)).to.equal(
+      50
+    );
+    expect(await token1.balanceOf(owner.address)).to.equal(
+      Number(token1AmountOfOwner) + 50
+    );
+
+    // deposits token1 into vault
+    await token2.connect(owner).approve(vault1.address, 1000);
+    await vault1.connect(owner).deposit(1000, token2.address);
+    const token2AmountOfOwner = await token2.balanceOf(owner.address);
+    await vault1.connect(owner).withdraw(750, token2.address);
+    expect(await vault1.tokenBalanceOf(owner.address, token2.address)).to.equal(
+      250
+    );
+    expect(await token2.balanceOf(owner.address)).to.equal(
+      Number(token2AmountOfOwner) + 750
+    );
+  });
+
+  it("withdrawing the exact amount should be possible", async () => {
+    // deposits token1 into vault
+    await token1.connect(owner).approve(vault1.address, 1000);
+    await vault1.connect(owner).deposit(100, token1.address);
+    expect(await vault1.tokenBalanceOf(owner.address, token1.address)).to.equal(
+      100
+    );
+    await vault1.connect(owner).withdraw(100, token1.address);
+    expect(await vault1.tokenBalanceOf(owner.address, token1.address)).to.equal(
+      0
+    );
+    // should be reverted with underflow
+    await expect(vault1.connect(owner).withdraw(500, token1.address)).to.be
+      .reverted;
+  });
+
+  it("withdrawing more than it's available, should revert", async () => {
+    // deposits token1 into vault
+    await token1.connect(owner).approve(vault1.address, 1000);
+    await vault1.connect(owner).deposit(100, token1.address);
+    // should be reverted with underflow
+    await expect(vault1.connect(owner).withdraw(500, token1.address)).to.be
+      .reverted;
+  });
+
+  it("should revert when tries to deposit 0", async function () {
+    await expect(
+      vault1.connect(owner).deposit(0, token1.address)
+    ).to.be.revertedWith("Amount can not be zero");
+  });
+
+  it("should revert when tries to withdraw 0", async function () {
+    await expect(
+      vault1.connect(owner).withdraw(0, token1.address)
+    ).to.be.revertedWith("Amount can not be zero");
+  });
 });
