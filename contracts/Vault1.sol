10d6e4e39c379988f42653d791b2d33114e4dd5a
Ted Wu <ted.wu@airbnb.com>
2022-01-19 22:39:47 -0800

    first stab at it

--- contracts/Vault1.sol
+++ contracts/Vault1.sol
@@ -1,25 +1,35 @@
 //SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.4;
 
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
 contract Vault1 {
-    address payable public owner;
-
+    string public constant name = "Vault1";
+    string public constant symbol = "VAULT1";
+    IERC20 public token;
+    
     constructor() {
-        owner = payable(msg.sender);
+        token = new ERC20(name, symbol);
     }
+    // events
+    event Deposit(uint _amount);
+    event Withdraw(uint _amount);
 
-    function deposit(uint _amount) public payable {
+    function deposit(uint _amount) payable external {
         require(_amount > 0, "Invalid amount to deposit");
+
+        token.transferFrom(msg.sender, address(this), _amount);
+
+        emit Deposit(_amount);
     }
 
-    function withdraw(uint _amount) public onlyOwner {
+    // prevent caller from withdrawing amount more than the balance
+    // function withdraw(uint _amount) public {
+    //     require(_balances[msg.sender] >= _amount, "Invalid amount to withdraw");
 
-    }
+    //     _balances[msg.sender] -= _amount;
 
-    // Modifier to check that the caller is the owner of
-    // the contract.
-    modifier onlyOwner() {
-        require(owner == msg.sender, "Not owner");
-        _;
-    }
+    //     emit Withdraw(_amount);
+    // }
 }
--- test/Vault1Test.js
+++ test/Vault1Test.js
@@ -12,10 +12,50 @@ describe("Vault 1", () => {
 	});
 
 	describe("deposit", () => {
-		it("should revert when the amount is zero", async function () {
+		it("should revert when the amount is zero", async function() {
 			await expect(
 				vault1.connect(account1).deposit(0)
 			).to.be.revertedWith("Invalid amount to deposit");
 		});
+		
+		it("correctly adds balance to the caller", async function() {
+			await vault1.connect(account1).deposit(10);
+
+			const accountBalance = await vault1.balanceOf(account1.address);
+			console.log("accountBalance", accountBalance);
+			await expect(accountBalance.toNumber()).to.eq(20);
+		});
+
+		// it("should emit deposit event when account deposits", async function () {
+		// 	await expect(vault1.connect(account1).deposit(10))
+		// 		.to.emit(vault1, "Deposit")
+		// 		.withArgs(10);
+		// });
+	});
+
+	describe("withdraw", () => {
+		// it("should revert when the withdraw amount is more than balance ", async function() {
+		// 	const currentBalance = await vault1.connect(account1).getBalance();
+		// 	console.log("currentBalance: ", currentBalance.toNumber());
+			
+		// 	await expect(
+		// 		vault1.connect(account1).withdraw(10)
+		// 	).to.be.revertedWith("Invalid amount to withdraw");
+		// });
+		
+		// it("correctly withdraws balance", async function() {
+		// 	await vault1.connect(account1).deposit(100);
+
+		// 	await vault1.connect(account1).withdraw(10);
+		// 	await expect(
+		// 		await vault1.connect(account1).getBalance()
+		// 	).to.eq("90");
+		// });
+
+		// it("should emit withdraw event when account deposits", async function () {
+		// 	await expect(vault1.connect(account1).withdraw(10))
+		// 		.to.emit(vault1, "Withdraw")
+		// 		.withArgs(10);
+		// });
 	});
 });
