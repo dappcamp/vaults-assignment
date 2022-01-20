6b320c8409c70587304464b64d0861026547be3a
Ted Wu <ted.wu@airbnb.com>
2022-01-19 20:28:17 -0800

    add vault 1

--- contracts/Vault1.sol
+++ contracts/Vault1.sol
@@ -2,5 +2,17 @@
 pragma solidity ^0.8.4;
 
 contract Vault1 {
-    
+    address payable public owner;
+
+    constructor() public {
+        owner = payable(msg.sender);
+    }
+
+    function deposit(uint _amount) public payable {
+
+    }
+
+    function withdraw(uint _amount) public {
+
+    }
 }
