f06085e4e49a162ab3524da1740131b41052ed66
Maria V <varvaroimaria@gmail.com>
2022-01-21 13:19:04 +0200

    vault1 + tests

--- contracts/Vault1.sol
+++ contracts/Vault1.sol
@@ -1,6 +1,49 @@
 //SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.4;
 
-contract Vault1 {
-    
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract Vault1  {
+    //mapping that the user exists
+    mapping(address => bool) public userExists; 
+
+    //mapping user => balance
+    mapping(address => uint) public balancesOf;
+
+    event Deposit(address user, uint amount);
+    event Withdraw(address owner, uint amount);
+
+    ERC20 public token;
+
+    constructor(address tokenAddress) {
+        token = ERC20(tokenAddress);
+    }
+
+    function deposit(uint256 _amount) public {
+        //check that the deposited amount is greater than 0
+        require(_amount > 0, "Deposit more than 0");
+        //add sender to the list of owners
+        userExists[msg.sender] = true;
+
+        //add the amount to the balance of the user
+        balancesOf[msg.sender] += _amount;
+
+
+        emit Deposit(msg.sender, _amount);
+    }
+
+    function withdraw(uint _amount) public  {
+        //check if the user exists
+        require(userExists[msg.sender], "You don't have a deposit here");
+
+        //check if the user has enough tokens in the account to withdraw the desired amount
+        require(balancesOf[msg.sender] >= _amount, "Not enough tokens" );
+
+        //withdraw the tokens 
+        balancesOf[msg.sender] -= _amount;
+
+        //emit Withdraw event
+        emit Withdraw(msg.sender, _amount);
+
+    }
 }
--- test/Vault1Test.js
+++ test/Vault1Test.js
@@ -2,12 +2,76 @@ const { expect } = require("chai");
 const { ethers } = require("hardhat");
 
 describe("Vault 1", () => {
-	beforeEach(async () => {
-		Vault1 = await ethers.getContractFactory("Vault1");
-		[owner] = await ethers.getSigners();
+  const tokenAddress = "0x6b175474e89094c44da98b954eedeac495271d0f";
+  beforeEach(async () => {
+    Vault1 = await ethers.getContractFactory("Vault1");
+    [user1, user2] = await ethers.getSigners();
 
-		vault1 = await Vault1.deploy();
+    vault1 = await Vault1.deploy(tokenAddress);
 
-		await vault1.deployed();
-	});
+    await vault1.deployed();
+  });
+  describe("deposit", function () {
+    it("should revert when amount is 0", async function () {
+      await expect(vault1.connect(user1).deposit(0)).to.be.revertedWith(
+        "Deposit more than 0"
+      );
+    });
+
+    it("should add the amount to the user's balance", async () => {
+      const tokenAmount = 4;
+      let initialBalance = await vault1.balancesOf(user1.address);
+      initialBalance = initialBalance.toNumber();
+      await vault1.connect(user1).deposit(tokenAmount);
+      let increasedBalance = await vault1.balancesOf(user1.address);
+      increasedBalance = increasedBalance.toNumber();
+      expect(increasedBalance).to.equal(initialBalance + tokenAmount);
+    });
+
+    it("should set userExists true", async () => {
+      let initialUserExists = await vault1.userExists(user1.address);
+      expect(initialUserExists).to.equal(false);
+      await vault1.connect(user1).deposit(2);
+      let userExists = await vault1.userExists(user1.address);
+      expect(userExists).to.equal(true);
+    });
+
+    it("should emit deposit event when token is deposited", async function () {
+      await expect(vault1.connect(user1).deposit(5))
+        .to.emit(vault1, "Deposit")
+        .withArgs(user1.address, 5);
+    });
+  });
+  describe("withdraw", function () {
+    it("should revert when the user is not an existing user", async function () {
+      await expect(vault1.connect(user2).withdraw(2)).to.be.revertedWith(
+        "You don't have a deposit here"
+      );
+    });
+
+    it("should revert if the user's balance is smaller than the amount to withdraw", async function () {
+      let amountToWithdraw = 5;
+      await vault1.connect(user1).deposit(4);
+      await expect(
+        vault1.connect(user1).withdraw(amountToWithdraw)
+      ).to.be.revertedWith("Not enough tokens");
+    });
+
+    it("should decrease the balance by the amount withdrawn", async () => {
+      let initialBalance = await vault1.balancesOf(user1.address);
+      initialBalance = initialBalance.toNumber();
+      await vault1.connect(user1).deposit(4);
+      await vault1.connect(user1).withdraw(3);
+      let finalBalance = await vault1.balancesOf(user1.address);
+      finalBalance = finalBalance.toNumber();
+      expect(finalBalance).to.equal(initialBalance + 1);
+    });
+
+    it("should emit Withdraw event when token is withdrawn", async function () {
+      await vault1.connect(user1).deposit(4);
+      await expect(vault1.connect(user1).withdraw(2))
+        .to.emit(vault1, "Withdraw")
+        .withArgs(user1.address, 2);
+    });
+  });
 });
