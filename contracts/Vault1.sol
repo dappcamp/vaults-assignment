2c0c4e3280918f008641978e413ca2975097b5df
John Mosesman <johnmosesman@gmail.com>
2022-01-21 14:17:58 -0600

    WIP

--- contracts/Vault1.sol
+++ contracts/Vault1.sol
@@ -1,6 +1,77 @@
 //SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.4;
 
-contract Vault1 {
-    
+import "hardhat/console.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract Tok1 is ERC20 {
+    constructor() ERC20("Token 1", "TOK1") {
+        _mint(msg.sender, 100_000);
+    }
+}
+
+contract Tok2 is ERC20 {
+    constructor() ERC20("Token 2", "TOK2") {
+        _mint(msg.sender, 100_000);
+    }
+}
+
+contract Vault {
+    mapping(address => mapping(address => uint256)) private _vaults;
+
+    function deposit(address _erc20TokenAddress, uint256 _amount) external {
+        require(
+            _erc20TokenAddress != address(0),
+            "Token address can't be zero address"
+        );
+        require(_amount > 0, "Amount must be > 0");
+
+        bool transferSucceeded = IERC20(_erc20TokenAddress).transferFrom(
+            msg.sender,
+            address(this),
+            _amount
+        );
+
+        require(transferSucceeded, "Transfer failed");
+
+        _vaults[msg.sender][_erc20TokenAddress] += _amount;
+        // emit event
+    }
+
+    function withdraw(address _erc20TokenAddress, uint256 _amount) external {
+        require(
+            _erc20TokenAddress != address(0),
+            "Token address can't be zero address"
+        );
+        require(_amount > 0, "Amount must be > 0");
+        require(
+            this.balanceOf(_erc20TokenAddress) >= _amount,
+            "Not enough tokens deposited"
+        );
+
+        bool transferSucceeded = IERC20(_erc20TokenAddress).transferFrom(
+            address(this),
+            msg.sender,
+            _amount
+        );
+
+        require(transferSucceeded, "Transfer failed");
+
+        _vaults[msg.sender][_erc20TokenAddress] -= _amount;
+        // emit event
+    }
+
+    function balanceOf(address _erc20TokenAddress)
+        public
+        view
+        returns (uint256)
+    {
+        require(
+            _erc20TokenAddress != address(0),
+            "Token address can't be zero address"
+        );
+
+        console.log("val: ", _vaults[msg.sender][_erc20TokenAddress]);
+        return _vaults[msg.sender][_erc20TokenAddress];
+    }
 }
--- test/Vault1Test.js
+++ test/Vault1Test.js
@@ -2,12 +2,147 @@ const { expect } = require("chai");
 const { ethers } = require("hardhat");
 
 describe("Vault 1", () => {
+	let tok1, tok2, vault;
+	let deployer;
+	let user1, user2;
+
 	beforeEach(async () => {
-		Vault1 = await ethers.getContractFactory("Vault1");
-		[owner] = await ethers.getSigners();
+		[deployer, user1, user2] = await ethers.getSigners();
 
-		vault1 = await Vault1.deploy();
+		// `deployer` deploys all the contracts
+		const Tok1 = await ethers.getContractFactory("Tok1", deployer);
+		tok1 = await Tok1.deploy();
+		await tok1.deployed();
 
-		await vault1.deployed();
+		const Tok2 = await ethers.getContractFactory("Tok2", deployer);
+		tok2 = await Tok2.deploy();
+		await tok2.deployed();
+
+		const Vault = await ethers.getContractFactory("Vault", deployer);
+		vault = await Vault.deploy();
+		await vault.deployed();
+
+		// Give user1 the two tokens
+		await tok1.connect(deployer).approve(user1.address, 1_000);
+		await tok1.connect(user1).transferFrom(deployer.address, user1.address, 1_000);
+
+		await tok2.connect(deployer).approve(user1.address, 1_000);
+		await tok2.connect(user1).transferFrom(deployer.address, user1.address, 1_000);
+
+		// Give user2 the two tokens
+		await tok1.connect(deployer).approve(user2.address, 1_000);
+		await tok1.connect(user2).transferFrom(deployer.address, user2.address, 1_000);
+
+		await tok2.connect(deployer).approve(user2.address, 1_000);
+		await tok2.connect(user2).transferFrom(deployer.address, user2.address, 1_000);
+
+		// From the prompt: "Assume that the contract is pre-approved to transfer that amount"
+		// Give the vault approval to transfer user1's tokens
+		await tok1.connect(user1).approve(vault.address, 1_000);
+		await tok2.connect(user1).approve(vault.address, 1_000);
+
+		// Give the vault approval to transfer user2's tokens
+		await tok1.connect(user2).approve(vault.address, 1_000);
+		await tok2.connect(user2).approve(vault.address, 1_000);
 	});
+
+	// describe("deposit", () => {
+	// 	it("requires a non-zero token address", async () => {
+	// 		await expect(
+	// 			vault.connect(user1).deposit(ethers.constants.AddressZero, 0)
+	// 		).to.be.revertedWith("Token address can't be zero address");
+	// 	});
+
+	// 	it("requires an amount > 0", async () => {
+	// 		await expect(
+	// 			vault.connect(user1).deposit(tok1.address, 0)
+	// 		).to.be.revertedWith("Amount must be > 0");
+	// 	});
+
+	// 	it("reverts if the user doesn't have that many tokens", async () => {
+	// 		await expect(
+	// 			vault.connect(user1).deposit(tok1.address, 20)
+	// 		).to.be.revertedWith("Not enough tokens");
+	// 	});
+
+	// 	it("stores the amount deposited for that token", async () => {
+	// 		await vault.connect(user1).deposit(tok1.address, 10);
+	// 		await vault.connect(user1).deposit(tok2.address, 5);
+
+	// 		expect(
+	// 			await vault.connect(user1).balanceOf(tok1.address)
+	// 		).to.equal(10);
+
+	// 		expect(
+	// 			await vault.connect(user1).balanceOf(tok2.address)
+	// 		).to.equal(5);
+
+	// 		await vault.connect(user2).deposit(tok1.address, 2);
+	// 		await vault.connect(user2).deposit(tok2.address, 3);
+
+	// 		expect(
+	// 			await vault.connect(user2).balanceOf(tok1.address)
+	// 		).to.equal(2);
+
+	// 		expect(
+	// 			await vault.connect(user2).balanceOf(tok2.address)
+	// 		).to.equal(3);
+	// 	});
+	// });
+
+	describe("withdraw", () => {
+		// it("requires a non-zero token address", async () => {
+		// 	await expect(
+		// 		vault.connect(user1).withdraw(ethers.constants.AddressZero, 0)
+		// 	).to.be.revertedWith("Token address can't be zero address");
+		// });
+
+		// it("requires an amount > 0", async () => {
+		// 	await expect(
+		// 		vault.connect(user1).withdraw(tok1.address, 0)
+		// 	).to.be.revertedWith("Amount must be > 0");
+		// });
+
+		// it("reverts if the user doesn't have that many tokens", async () => {
+		// 	await expect(
+		// 		vault.connect(user1).withdraw(tok1.address, 20)
+		// 	).to.be.revertedWith("Not enough tokens deposited");
+		// });
+
+		it("withdraws the tokens if the user has deposited them", async () => {
+			const txn = await vault.connect(user1).deposit(tok1.address, 10);
+			await txn.wait();
+
+			await vault.connect(user1).withdraw(tok1.address, 5);
+
+			expect(await vault.balanceOf(tok1.address)).to.equal(5);
+			expect(await tok1.balanceOf(user1.address)).to.equal(995);
+		});
+	});
+
+	// describe("balanceOf", () => {
+	// 	it("reverts if the token address is the zero address", async () => {
+	// 		await expect(
+	// 			vault.connect(user1).balanceOf(ethers.constants.AddressZero)
+	// 		).to.be.revertedWith("Token address can't be zero address");
+	// 	});
+
+	// 	it("returns 0 if the user hasn't deposited any tokens", async () => {
+	// 		expect(
+	// 			await vault.connect(user1).balanceOf(tok1.address)
+	// 		).to.equal(0);
+	// 	});
+
+	// 	it("returns the balance", async () => {
+	// 		await vault.connect(user1).deposit(tok1.address, 10);
+
+	// 		expect(
+	// 			await vault.connect(user1).balanceOf(tok1.address)
+	// 		).to.equal(10);
+
+	// 		expect(
+	// 			await vault.connect(user1).balanceOf(tok2.address)
+	// 		).to.equal(0);
+	// 	});
+	// });
 });
