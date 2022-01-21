6d113205786c06d909c56f504ad63bf2067ed0ff
Maria V <varvaroimaria@gmail.com>
2022-01-21 18:29:58 +0200

    vault2

--- contracts/Vault2.sol
+++ contracts/Vault2.sol
@@ -1,6 +1,37 @@
 //SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.4;
 
-contract Vault2 {
-    
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract Vault2 is ERC20 {
+    //checks if the user has an account or not
+    mapping(address => bool) hasAccount;
+    event Mint(uint256 amount);
+    event Burn(uint256 amount);
+
+    constructor() payable ERC20("VaultToken", "VAULT") {}
+
+    receive() external payable {}
+
+    function mint() public payable {
+        require(msg.value > 0, "Please deposit more than 0 tokens");
+        _mint(msg.sender, msg.value);
+        emit Mint(msg.value);
+    }
+
+    function burn(uint256 _amount) public payable {
+        require(
+            _amount <= balanceOf(msg.sender),
+            "Not enough VAULT tokens to burn"
+        );
+
+        _burn(msg.sender, _amount);
+        payable(msg.sender).transfer(_amount);
+
+        emit Burn(_amount);
+    }
+
+    function balanceOfEth() public view returns (uint256) {
+        return address(this).balance;
+    }
 }
--- test/Vault2Test.js
+++ test/Vault2Test.js
@@ -1,13 +1,92 @@
 const { expect } = require("chai");
+const { BigNumber } = require("ethers");
 const { ethers } = require("hardhat");
 
 describe("Vault 2", () => {
-	beforeEach(async () => {
-		Vault2 = await ethers.getContractFactory("Vault2");
-		[owner] = await ethers.getSigners();
+  beforeEach(async () => {
+    Vault2 = await ethers.getContractFactory("Vault2");
+    [owner, owner2] = await ethers.getSigners();
 
-		vault2 = await Vault2.deploy();
+    vault2 = await Vault2.deploy();
 
-		await vault2.deployed();
-	});
+    await vault2.deployed();
+  });
+
+  describe("mint", function () {
+    it("should revert when amount is 0", async function () {
+      await expect(
+        vault2.connect(owner).mint({ value: ethers.utils.parseEther("0") })
+      ).to.be.revertedWith("Please deposit more than 0 tokens");
+    });
+
+    it("should mint an given amount of tokens", async () => {
+      let initialBalance = await vault2.balanceOf(owner.address);
+      initialBalance = initialBalance.toNumber();
+      await vault2
+        .connect(owner)
+        .mint({ value: ethers.utils.parseUnits("5", "gwei") });
+      let increasedBalance = await vault2.balanceOf(owner.address);
+      increasedBalance = increasedBalance.toNumber();
+      expect(increasedBalance).to.equal(initialBalance + 5000000000);
+    });
+    it("should emit Mint event when token is minted", async function () {
+      await expect(
+        vault2
+          .connect(owner)
+          .mint({ value: ethers.utils.parseUnits("500", "wei") })
+      )
+        .to.emit(vault2, "Mint")
+        .withArgs(500);
+    });
+  });
+  describe("burn", function () {
+    it("should revert when amount is less than balance", async function () {
+      await vault2
+        .connect(owner)
+        .mint({ value: ethers.utils.parseUnits("500", "wei") });
+      let currentBalance = await vault2.balanceOf(owner.address);
+      currentBalance = currentBalance.toNumber();
+      await expect(vault2.connect(owner).burn(600)).to.be.revertedWith(
+        "Not enough VAULT tokens to burn"
+      );
+    });
+
+    it("should burn a given amount of tokens", async () => {
+      await vault2
+        .connect(owner)
+        .mint({ value: ethers.utils.parseUnits("500", "wei") });
+      await vault2.connect(owner).burn(400);
+      let decreasedBalance = await vault2.balanceOf(owner.address);
+      decreasedBalance = decreasedBalance.toNumber();
+      expect(decreasedBalance).to.equal(100);
+    });
+
+    it("should emit Burn event when token is burned", async function () {
+      await vault2
+        .connect(owner)
+        .mint({ value: ethers.utils.parseUnits("500", "wei") });
+      await expect(vault2.connect(owner).burn(100))
+        .to.emit(vault2, "Burn")
+        .withArgs(100);
+    });
+  });
+
+  describe("balanceOfEth", function () {
+    it("should show total balance of ether", async () => {
+      let initialBalance = await vault2.balanceOfEth();
+      initialBalance = initialBalance.toNumber();
+      await vault2
+        .connect(owner)
+        .mint({ value: ethers.utils.parseUnits("500", "wei") });
+      let increasedBalanceOwner1 = await vault2.balanceOfEth();
+      increasedBalanceOwner1 = increasedBalanceOwner1.toNumber();
+      expect(increasedBalanceOwner1).to.equal(initialBalance + 500);
+      await vault2
+        .connect(owner2)
+        .mint({ value: ethers.utils.parseUnits("600", "wei") });
+      let increasedBalanceOwner2 = await vault2.balanceOfEth();
+      increasedBalanceOwner2 = increasedBalanceOwner2.toNumber();
+      expect(increasedBalanceOwner2).to.equal(initialBalance + 500 + 600);
+    });
+  });
 });
