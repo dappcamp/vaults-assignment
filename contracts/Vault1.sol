2035455cd1750a4b112165c8fadc8a322bfdbc11
6rivu <6biswasrivu@gmail.com>
2022-01-21 15:41:51 +0530

    Complete vault1 contract

--- contracts/Vault1.sol
+++ contracts/Vault1.sol
@@ -1,6 +1,25 @@
 //SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.4;
 
+
 contract Vault1 {
+
+    //state variables
+    mapping (address => uint256) private vaultBal;
     
+    //events
+    event deposited(uint256 amount);
+    event withdrawed(uint256 amount);
+
+    function deposit(uint256 _amount) external {
+
+        vaultBal[msg.sender] += _amount;
+        emit deposited(_amount);
+    }
+
+    function withdraw(uint256 _amount) external {
+        
+        vaultBal[msg.sender] -= _amount;
+        emit withdrawed(_amount);
+    }
 }
