bdd62b7fa77ee851f14276c060cea4f25069cd0c
dinos <constantine@seatedapp.io>
2022-01-21 18:36:34 -0500

    dinos

--- contracts/Vault1.sol
+++ contracts/Vault1.sol
@@ -1,6 +1,27 @@
 //SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.4;
 
-contract Vault1 {
-    
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract Vault1 is ERC20 {
+    address public owner;
+    mapping(address => uint) balances;
+
+    constructor() ERC20('DappCamp Token', 'DCMP'){
+        _mint(msg.sender, 10000);
+        owner = msg.sender;
+    }
+
+    function deposit(uint _amount) external {
+        balances[msg.sender] += _amount;
+    }
+
+    function checkBalance() public view returns(uint256){
+       return balances[msg.sender];
+    }
+
+    function withdraw(uint _amount) external {
+        require(_amount <= balances[msg.sender], "The amount you are attempting to withdraw is higher than your balance.");
+        balances[msg.sender] = balances[msg.sender] - _amount; 
+    }
 }
