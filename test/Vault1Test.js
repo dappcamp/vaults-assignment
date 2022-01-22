2fd962e771bb105f62ff19bece559d8ebfadce4d
pardhaponugoti <pardhapon@gmail.com>
2022-01-21 20:10:57 -0800

    organize tests

--- test/Vault1Test.js
+++ test/Vault1Test.js
@@ -22,43 +22,47 @@ describe("Vault 1", () => {
 	});
 
 	// Depositing
-	it("should emit a Deposit event when a deposit is successful", async () => {
-		await token1.connect(elon).approve(vault1.address, 100);
-		await expect(vault1.connect(elon).deposit(100, token1.address))
-			.to.emit(vault1, "Deposit")
-			.withArgs(elon.address, 100, token1.address);
+	describe("Depositing", () => {
+		it("should emit a Deposit event when a deposit is successful", async () => {
+			await token1.connect(elon).approve(vault1.address, 100);
+			await expect(vault1.connect(elon).deposit(100, token1.address))
+				.to.emit(vault1, "Deposit")
+				.withArgs(elon.address, 100, token1.address);
 
-		const vaultBalance = await token1.balanceOf(vault1.address);
-		expect(vaultBalance).to.eq(100);
-	})
+			const vaultBalance = await token1.balanceOf(vault1.address);
+			expect(vaultBalance).to.eq(100);
+		})
 
-	it("should revert when a user tries to deposit more than they have", async () => {
-		await token1.connect(elon).approve(vault1.address, 10000);
-		await expect(vault1.connect(elon).deposit(10000, token1.address))
-			.to.be.revertedWith("Insufficient balance")
+		it("should revert when a user tries to deposit more than they have", async () => {
+			await token1.connect(elon).approve(vault1.address, 10000);
+			await expect(vault1.connect(elon).deposit(10000, token1.address))
+				.to.be.revertedWith("Insufficient balance")
+		})
 	})
 		
 	// Withdrawing
-	it("should emit a Withdraw event when a withdraw is successful", async () => {
-		await token1.connect(elon).approve(vault1.address, 100);
-		await vault1.connect(elon).deposit(100, token1.address);
-		
-		const vaultBalance = await token1.balanceOf(vault1.address);
-		expect(vaultBalance).to.eq(100);
-		
-		await expect(vault1.connect(elon).withdraw(100, token1.address))
-			.to.emit(vault1, "Withdraw")
-			.withArgs(elon.address, 100, token1.address);
-	})
-		
-	it("should revert when a user tries to withdraw more than they have stored", async () => {
-		await token1.connect(elon).approve(vault1.address, 100);
-		await vault1.connect(elon).deposit(100, token1.address);
-		
-		const vaultBalance = await token1.balanceOf(vault1.address);
-		expect(vaultBalance).to.eq(100);
-		
-		await expect(vault1.connect(elon).withdraw(101, token1.address))
-			.to.be.revertedWith("Insufficient balance")
+	describe("Withdrawing", () => {
+		it("should emit a Withdraw event when a withdraw is successful", async () => {
+			await token1.connect(elon).approve(vault1.address, 100);
+			await vault1.connect(elon).deposit(100, token1.address);
+			
+			const vaultBalance = await token1.balanceOf(vault1.address);
+			expect(vaultBalance).to.eq(100);
+			
+			await expect(vault1.connect(elon).withdraw(100, token1.address))
+				.to.emit(vault1, "Withdraw")
+				.withArgs(elon.address, 100, token1.address);
+		})
+			
+		it("should revert when a user tries to withdraw more than they have stored", async () => {
+			await token1.connect(elon).approve(vault1.address, 100);
+			await vault1.connect(elon).deposit(100, token1.address);
+			
+			const vaultBalance = await token1.balanceOf(vault1.address);
+			expect(vaultBalance).to.eq(100);
+			
+			await expect(vault1.connect(elon).withdraw(101, token1.address))
+				.to.be.revertedWith("Insufficient balance")
+		})
 	})
 });
--- test/Vault2Test.js
+++ test/Vault2Test.js
@@ -13,36 +13,40 @@ describe("Vault 2", () => {
 	});
 
 	// Minting
-	it("Should allow users to exchange ethereum for VAULT", async () => {
-		await expect(vault2.connect(preethi).mint(100, { value: 100 }))
-			.to.emit(vault2, "Mint").withArgs(preethi.address, 100);
+	describe("Minting", () => {
+		it("Should allow users to exchange ethereum for VAULT", async () => {
+			await expect(vault2.connect(preethi).mint(100, { value: 100 }))
+				.to.emit(vault2, "Mint").withArgs(preethi.address, 100);
 
-		const finalUserWethBalance = await vault2.balanceOf(preethi.address);
-		const finalVaultWethBalance = await vault2.balanceOf(vault2.address);
+			const finalUserWethBalance = await vault2.balanceOf(preethi.address);
+			const finalVaultWethBalance = await vault2.balanceOf(vault2.address);
 
-		expect(finalUserWethBalance).to.eq(100);
-		expect(initialVaultWethBalance - finalVaultWethBalance).to.eq(100);
-	})
+			expect(finalUserWethBalance).to.eq(100);
+			expect(initialVaultWethBalance - finalVaultWethBalance).to.eq(100);
+		})
 
-	it("Should not allow users to pass unequal amounts of amount and value", async () => {
-		await expect(vault2.connect(preethi).mint(100, { value: 1000 }))
-			.to.be.revertedWith("Invalid amount");
+		it("Should not allow users to pass unequal amounts of amount and value", async () => {
+			await expect(vault2.connect(preethi).mint(100, { value: 1000 }))
+				.to.be.revertedWith("Invalid amount");
+		})
 	})
 
 	// Burning
-	it("Should allow users to burn their VAULT for an equal amount of ETH", async () => {
-		await vault2.connect(preethi).mint(100, { value: 100 });
+	describe("Burning", () => {
+		it("Should allow users to burn their VAULT for an equal amount of ETH", async () => {
+			await vault2.connect(preethi).mint(100, { value: 100 });
 
-		await vault2.connect(preethi).approve(vault2.address, 10);
-		await expect(vault2.connect(preethi).burn(10))
-			.to.emit(vault2, "Burn").withArgs(preethi.address, 10);
-	})
+			await vault2.connect(preethi).approve(vault2.address, 10);
+			await expect(vault2.connect(preethi).burn(10))
+				.to.emit(vault2, "Burn").withArgs(preethi.address, 10);
+		})
 
-	it("Should not allow users to burn more VAULT than they have", async () => {
-		await vault2.connect(preethi).mint(100, { value: 100 });
+		it("Should not allow users to burn more VAULT than they have", async () => {
+			await vault2.connect(preethi).mint(100, { value: 100 });
 
-		await vault2.connect(preethi).approve(vault2.address, 10000);
-		await expect(vault2.connect(preethi).burn(10000))
-			.to.be.revertedWith("Invalid amount")
+			await vault2.connect(preethi).approve(vault2.address, 10000);
+			await expect(vault2.connect(preethi).burn(10000))
+				.to.be.revertedWith("Invalid amount")
+		})
 	})
 });
