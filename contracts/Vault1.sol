265620ebc2d8de6efdecffe9b02da24531bc8e94
Tyler <divergio@gmail.com>
2022-01-22 01:10:13 -0800

    Vault1 and Vault2 completed

--- contracts/Vault1.sol
+++ contracts/Vault1.sol
@@ -1,6 +1,31 @@
 //SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.4;
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
 
 contract Vault1 {
-    
+    IERC20 public token;
+
+    mapping(address => uint) public vaultBalances;
+
+    constructor(address _erc20Contract) {
+        token = IERC20(_erc20Contract);
+        token.approve(address(this), type(uint).max);
+    }
+
+    function deposit(uint _amount) external {
+        require(_amount > 0, "Invalid deposit amount");
+        require(token.balanceOf(msg.sender) >= _amount, "Insufficient balance");
+
+        token.transferFrom(msg.sender, address(this), _amount);
+        vaultBalances[msg.sender] = _amount;
+    }
+
+    function withdraw (uint _amount) external {
+        require(_amount > 0, "Invalid withdraw amount");
+        require(vaultBalances[msg.sender] >= _amount, "Insufficient balance in vault");
+
+        vaultBalances[msg.sender] -= _amount;
+        token.transferFrom(address(this), msg.sender, _amount);
+    }
 }
--- test/Vault1Test.js
+++ test/Vault1Test.js
@@ -4,10 +4,86 @@ const { ethers } = require("hardhat");
 describe("Vault 1", () => {
 	beforeEach(async () => {
 		Vault1 = await ethers.getContractFactory("Vault1");
-		[owner] = await ethers.getSigners();
+		Token = await ethers.getContractFactory("Token");
 
-		vault1 = await Vault1.deploy();
+		const accounts = await ethers.getSigners();
+		owner = accounts[0];
+		account1 = accounts[1];
+		account2 = accounts[2];
 
+		token = await Token.deploy(); 		
+		await token.deployed();
+
+		vault1 = await Vault1.deploy(token.address);
 		await vault1.deployed();
+
+	});
+
+	describe("deposit", function () {
+		beforeEach(async () => {
+			await token.transfer(account1.address, 500);
+
+			// spec "says Assume that the contract is pre-approved to transfer that amount"
+			await token.connect(account1).approve(vault1.address, 250);
+		});
+
+		it("deposit should not work with 0 amount", async function () {
+			await expect(vault1.connect(account1).deposit(0)).to.be.revertedWith("Invalid deposit amount");
+		});
+
+		// Ethers won't let me provide an invalid negative amount
+		// it("deposit should not work with negative amount", async function () {
+		// 	expect(vault1.connect(account1).deposit(-10)).to.be.revertedWith("Invalid deposit amount: negative");
+		// });
+
+		it("deposit should not work with amount exceeding how much they own", async function () {
+			await expect(vault1.connect(account1).deposit(501)).to.be.revertedWith("Insufficient balance");
+		});
+
+		it("deposit should remove coins from account and put in vault", async function () {
+			await vault1.connect(account1).deposit(250);
+
+			const account1Balance = await token.balanceOf(account1.address);
+			expect(account1Balance).to.equal(250);
+
+			const tokenAmount = await vault1.vaultBalances(account1.address);
+			expect(tokenAmount).to.equal(250);
+		});
+
+	});
+
+	describe("withdraw", function () {
+		beforeEach(async () => {
+			await token.transfer(account1.address, 500);
+
+			// TODO: from a testing standpoint, is it better if I directly change the state of the vault
+			// and then run the withdraw functions, rather than calling deposit on it here? 
+			await token.connect(account1).approve(vault1.address, 250);
+			await vault1.connect(account1).deposit(250);
+
+		});
+
+		it("withdraw should not work with 0 amount", async function () {
+			await expect(vault1.connect(account1).withdraw(0)).to.be.revertedWith("Invalid withdraw amount");
+		});
+
+		it("should not be able to withdraw more than deposited", async function () {
+			await expect(vault1.connect(account1).withdraw(251)).to.be.revertedWith("Insufficient balance in vault");
+		});
+
+		// Is this test unnecessary? 
+		it("someone else should not be able to withdraw the coins", async function () {
+			await expect(vault1.connect(account2).withdraw(1)).to.be.revertedWith("Insufficient balance in vault");
+		});
+
+		it("withdraw should remove coins from vault and go back to sender", async function () {
+			await vault1.connect(account1).withdraw(125);
+
+			const account1Balance = await token.balanceOf(account1.address);
+			expect(account1Balance).to.equal(375);
+
+			const tokenAmount = await vault1.vaultBalances(account1.address);
+			expect(tokenAmount).to.equal(125);
+		});
 	});
 });
