542dec2290fcdc62446310ac600cd694c665ad0f
Gabriel Song <gabrielsong@Gabriels-MacBook-Pro-14.local>
2022-01-22 00:41:22 -0800

    comment the plan

--- contracts/Vault1.sol
+++ contracts/Vault1.sol
@@ -4,16 +4,25 @@ pragma solidity ^0.8.4;
 import "./Owner.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-contract Vault1 is ERC20, Owner {
+contract Vault1 {
 
-    mapping(address => mapping(string => uint)) private holdings;
+    // state storage should have the amounts of different ERC20s deposited by each address
+    // addr1 -> (DAI, 14)
+    //       -> (UNI, 10)
+
+    // store map of symbols to contract addresses?
     
-    function deposit(_amount) external returns (bool) {
-        require(_amount > 0, "amount should be greater than 0");
-        return transfer(address(this), _amount);
+    function deposit(uint _amount, string memory _symbol) public {
+        // must be able to identify the ERC20 that is being deposited
+        // must be able to find the ERC20 contract and call the transfer function
+        // keep track of deposited amounts for the sender
     }
 
-    function withdraw(_amount) external {
-        // TODO
+    function withdraw(uint _amount) public {
+        // must be able to identify the ERC20 that is being requested
+        // check the contract balance of the sender
+        // amount should be less than the sender's balance in the contract 
+        // call the ERC20 contract transfer function
+        // keep track of the withdrawal
     }
 }
