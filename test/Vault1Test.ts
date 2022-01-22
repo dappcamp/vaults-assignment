aed1884df188abb871ada5de5a8c7a207560755a
Ryan LaBouve <imryanlabouve@gmail.com>
2022-01-22 10:19:22 -0600

    Move to ts

--- test/Vault1Test.ts
+++ test/Vault1Test.ts
@@ -1,14 +1,17 @@
+import { Contract, ContractFactory, Signer } from "ethers";
+
 const { expect } = require("chai");
 const { ethers } = require("hardhat");
 
 describe("Vault 1", () => {
 
-	let Vault1;
-	let vault1Token;
-	let bottleCapsFactory;
-	let bottleCapsToken;
-	let addr1;
-	let addr2;
+	let Vault1: ContractFactory;
+	let vault1Token: Contract;
+	let bottleCapsFactory: ContractFactory;
+	let bottleCapsToken: Contract;
+	let owner: Signer;
+	let addr1: Signer;
+	let addr2: Signer;
 
 	beforeEach(async () => {
 		[owner, addr1, addr2] = await ethers.getSigners();
@@ -26,34 +29,37 @@ describe("Vault 1", () => {
 
 	describe("Deployment", async function () {
 		it("Should assign the total supply of tokens to the owner", async function () {
-			const ownerBalance = await bottleCapsToken.balanceOf(owner.address);
+			const ownerBalance = await bottleCapsToken.balanceOf(await owner.getAddress());
 			expect(await bottleCapsToken.totalSupply()).to.equal(ownerBalance);
 		});
 	});
 
-	describe("#deposit", async function() {
-		it("should take in a deposit amount", async function() {
-			await bottleCapsToken.connect(owner).transfer(addr1.address, 3); // Have some caps to transfer
+	describe("#deposit", async function () {
+		it("should take in a deposit amount", async function () {
+			let addr1Address: string = await addr1.getAddress();
+			await bottleCapsToken.connect(owner).transfer(addr1Address, 3); // Have some caps to transfer
 
 			await bottleCapsToken.connect(addr1).approve(vault1Token.address, 1);
 			await vault1Token.connect(addr1).deposit(1);
 
-			expect(await vault1Token.balanceOf(addr1.address)).to.eq(1);
-			expect(await bottleCapsToken.balanceOf(addr1.address)).to.eq(2);
+			expect(await vault1Token.balanceOf(addr1Address)).to.eq(1);
+			expect(await bottleCapsToken.balanceOf(addr1Address)).to.eq(2);
 		})
 	});
 
-	describe("#withdraw", async function() {
-		it("should allow users to widthdraw <= what's deposited", async function() {
-			await bottleCapsToken.connect(owner).transfer(addr1.address, 3); 
+	describe("#withdraw", async function () {
+		it("should allow users to withdraw <= what's deposited", async function () {
+			let addr1Address: string = await addr1.getAddress();
+			await bottleCapsToken.connect(owner).transfer(addr1Address, 3);
 			await bottleCapsToken.connect(addr1).approve(vault1Token.address, 1);
 			await vault1Token.connect(addr1).deposit(1);
 			await vault1Token.connect(addr1).withdraw(1);
-			expect(await vault1Token.balanceOf(addr1.address)).to.eq(0);
-			expect(await bottleCapsToken.balanceOf(addr1.address)).to.eq(3);
+			expect(await vault1Token.balanceOf(addr1Address)).to.eq(0);
+			expect(await bottleCapsToken.balanceOf(addr1Address)).to.eq(3);
 		});
-		it("should not allow overdrafts", async function() {
-			await bottleCapsToken.connect(owner).transfer(addr1.address, 3); 
+		it("should not allow overdrafts", async function () {
+			let addr1Address: string = await addr1.getAddress();
+			await bottleCapsToken.connect(owner).transfer(addr1Address, 3);
 			await bottleCapsToken.connect(addr1).approve(vault1Token.address, 1);
 			await vault1Token.connect(addr1).deposit(1);
 
--- test/Vault2Test.ts
+++ test/Vault2Test.ts
@@ -1,7 +1,13 @@
 const { expect } = require("chai");
 const { ethers } = require("hardhat");
 
+import { Contract, ContractFactory, Signer } from "ethers";
+
 describe("Vault 2", () => {
+	let Vault2: ContractFactory;
+	let vault2: Contract;
+	let owner: Signer;
+
 	beforeEach(async () => {
 		Vault2 = await ethers.getContractFactory("Vault2");
 		[owner] = await ethers.getSigners();
