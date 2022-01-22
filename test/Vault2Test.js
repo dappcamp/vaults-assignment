cafccd668b471ec398ff9902e3a7544f2fe95981
pardhaponugoti <pardhapon@gmail.com>
2022-01-21 20:14:45 -0800

    Add more conditions in tests

--- test/Vault2Test.js
+++ test/Vault2Test.js
@@ -20,33 +20,43 @@ describe("Vault 2", () => {
 
 			const finalUserWethBalance = await vault2.balanceOf(preethi.address);
 			const finalVaultWethBalance = await vault2.balanceOf(vault2.address);
-
+			
 			expect(finalUserWethBalance).to.eq(100);
 			expect(initialVaultWethBalance - finalVaultWethBalance).to.eq(100);
 		})
-
+		
 		it("Should not allow users to pass unequal amounts of amount and value", async () => {
 			await expect(vault2.connect(preethi).mint(100, { value: 1000 }))
-				.to.be.revertedWith("Invalid amount");
+			.to.be.revertedWith("Invalid amount");
 		})
 	})
-
+	
 	// Burning
 	describe("Burning", () => {
 		it("Should allow users to burn their VAULT for an equal amount of ETH", async () => {
 			await vault2.connect(preethi).mint(100, { value: 100 });
+			
+			const interimUserWethBalance = await vault2.balanceOf(preethi.address);
+			const interimVaultWethBalance = await vault2.balanceOf(vault2.address);
 
 			await vault2.connect(preethi).approve(vault2.address, 10);
 			await expect(vault2.connect(preethi).burn(10))
-				.to.emit(vault2, "Burn").withArgs(preethi.address, 10);
-		})
+			.to.emit(vault2, "Burn").withArgs(preethi.address, 10);
 
+			const finalUserWethBalance = await vault2.balanceOf(preethi.address);
+			const finalVaultWethBalance = await vault2.balanceOf(vault2.address);
+
+			expect(finalVaultWethBalance - interimVaultWethBalance).to.eq(10);
+			expect(interimUserWethBalance - finalUserWethBalance).to.eq(10);
+		})
+		
 		it("Should not allow users to burn more VAULT than they have", async () => {
 			await vault2.connect(preethi).mint(100, { value: 100 });
 
 			await vault2.connect(preethi).approve(vault2.address, 10000);
 			await expect(vault2.connect(preethi).burn(10000))
 				.to.be.revertedWith("Invalid amount")
+
 		})
 	})
 });
