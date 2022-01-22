21249875142db0af54767c5fa190b3d69af2e6b9
zliyclh <zliyclh@gmail.com>
2022-01-22 20:22:00 +0800

    haven't got a chance to work on test just yet but will do

--- contracts/Vault2.sol
+++ contracts/Vault2.sol
@@ -1,6 +1,32 @@
-//SPDX-License-Identifier: Unlicense
+// SPDX-License-Identifier: MIT
+
 pragma solidity ^0.8.4;
 
-contract Vault2 {
-    
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract Vault2 is ERC20 {
+    mapping (address => uint) public balances; // mapping from user address to deposited amount
+
+    constructor() ERC20("DAPP vault", "VAULT"){
+
+    }
+
+    function mint(uint _amount) external payable {
+        (bool success, ) = payable(address(this)).call { value : _amount }("");
+        require(success, "failed to transfer via call");
+
+        _mint(msg.sender, _amount);
+    }
+
+    function burn(uint _amount) external {
+        (bool success, ) = payable(msg.sender).call { value : _amount }("");
+        require(success, "failed to transfer via call");
+
+        _burn(msg.sender, _amount);
+
+
+    }
+
+
+
 }
