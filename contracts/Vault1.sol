4e12f9d4e4667a930adeb06e68c1a3250bf4b5b2
sid <sid@sids-MBP.lan>
2022-01-22 06:16:54 -0600

    vault1 and most of vault2

--- contracts/Vault1.sol
+++ contracts/Vault1.sol
@@ -1,6 +1,39 @@
 //SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.4;
 
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
 contract Vault1 {
+
+    ERC20 token;
+    //mapping of address to balance
+    mapping(address => uint256) public balance;
+
+    //contructor to set token type accepted by the vault
+constructor () {
     
+       token =  ERC20(0xc778417E063141139Fce010982780140Aa0cD5Ab);
+
+    }
+
+    function deposit(uint256 _amount) public
+    {
+
+        require(_amount > 0, "Enter valid amount");
+        token.transferFrom(msg.sender, address(this), _amount);
+        balance[msg.sender]+= _amount;
+
+    } 
+
+    function withdraw(uint256 _amount) public 
+    {
+
+        require(_amount > 0, "Enter valid amount");
+        require(_amount <= balance[msg.sender], "Withdrawal amount too high");
+        token.transferFrom(address(this),msg.sender, _amount);
+        balance[msg.sender]-= _amount;
+
+
+    }
+
 }
--- hardhat.config.js
+++ hardhat.config.js
@@ -1,21 +1,24 @@
 require("@nomiclabs/hardhat-waffle");
 
-// This is a sample Hardhat task. To learn how to create your own go to
-// https://hardhat.org/guides/create-task.html
-task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
-  const accounts = await hre.ethers.getSigners();
+// Go to https://www.alchemyapi.io, sign up, create
+// a new App in its dashboard, and replace "KEY" with its key
+const ALCHEMY_API_KEY = "G7xKJET0XOUE3UAReD2n4uHyRxm7VBb9";
 
-  for (const account of accounts) {
-    console.log(account.address);
-  }
-});
-
-// You need to export an object to set up your config
-// Go to https://hardhat.org/config/ to learn more
+// Replace this private key with your Ropsten account private key
+// To export your private key from Metamask, open Metamask and
+// go to Account Details > Export Private Key
+// Be aware of NEVER putting real Ether into testing accounts
+const RINKEBY_PRIVATE_KEY = "aebc98190df10761f3b19deb6d488693899dd7f4a058874acea5d5246324be34";
 
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
   solidity: "0.8.4",
+  networks: {
+   rinkeby: {
+    url: `https://eth-rinkeby.alchemyapi.io/v2/${ALCHEMY_API_KEY}`,
+    accounts: [`${RINKEBY_PRIVATE_KEY}`]
+    }
+  }
 };
