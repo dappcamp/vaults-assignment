e055f567572c920ae1871b233d6b3de209de8d1a
nageshannamareddy <nagesh.annamareddy@gmail.com>
2022-01-22 22:47:55 +0530

    Vault 1 Solidity changes and Testcases

--- test/Vault1Test.js
+++ test/Vault1Test.js
@@ -10,4 +10,67 @@ describe("Vault 1", () => {
 
 		await vault1.deployed();
 	});
+
+	describe("deposit", function () {
+		it("Deposit amount should be greater than zero", async function () {
+			await expect(
+				vault1.connect(owner).deposit(0)
+			).to.be.revertedWith("Deposit amount should be greater than zero");
+		});
+
+		it("should increment the account balance", async function () {
+			//sample amount to be deposited 
+			let depositAmount = 7;
+
+			//get balance before deposit
+			const beforeBalance = await vault1.balanceOf(owner.address);
+			
+			// deposit amount
+			await vault1.connect(owner).deposit(depositAmount);
+	
+			//get balance after deposit 
+			const afterBalance = await vault1.balanceOf(owner.address);
+			
+			//check if the balance increased propotional to deposited amount 
+			expect(afterBalance).to.equal(beforeBalance + depositAmount);
+		});
+	});
+
+	describe("withdraw", function () {
+		it("Withdraw amount should be greater than zero", async function () {
+			await expect(
+				vault1.connect(owner).withdraw(0)
+			).to.be.revertedWith("Withdraw amount should be greater than zero");
+		});
+
+		it("withdraw amount should be lesser than or equal to deposited amount", async function () {			
+			//get balance before withdraw
+			const balance = await vault1.balanceOf(owner.address);
+			
+			// try to withdraw amount greater than balance
+			await expect(
+				vault1.connect(owner).withdraw(balance + 1)
+			).to.be.revertedWith("withdraw amount should be lesser than or equal to deposited amount");
+		});
+
+		it("should decrement the account balance", async function () {			
+			//deposit sufficent funds to test withdraw
+			await vault1.connect(owner).deposit(1000);
+			
+			//sample amount to be withdraw 
+			let withdrawAmount = 5;
+
+			//get balance before withdraw
+			const beforeBalance = await vault1.balanceOf(owner.address);
+			
+			// withdraw amount
+			await vault1.connect(owner).withdraw(withdrawAmount);
+	
+			//get balance after withdraw 
+			const afterBalance = await vault1.balanceOf(owner.address);
+			
+			//check if the balance decreased propotional to withdraw amount 
+			expect(afterBalance).to.equal(beforeBalance - withdrawAmount);
+		});
+	});
 });
