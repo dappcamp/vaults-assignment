acd4ff1610a790d551d6117edb52daff46b3e019
Ryan LaBouve <imryanlabouve@gmail.com>
2022-01-22 00:30:38 -0600

    WIP

--- contracts/Vault1.sol
+++ contracts/Vault1.sol
@@ -1,6 +1,23 @@
 //SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.4;
 
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "./BottleCaps.sol";
+
 contract Vault1 {
-    
+    BottleCaps public caps;
+    mapping(address => uint256) public balanceOf;
+
+    constructor() {}
+
+    function deposit() external payable {
+        caps.transferFrom(msg.sender, address(this), msg.value);
+        balanceOf[msg.sender] += msg.value;
+    }
+
+    function withdraw(uint256 amount) public {
+        // how to actually transfer
+        caps.transferFrom(address(this), msg.sender, amount);
+        balanceOf[msg.sender] -= amount;
+    }
 }
--- test/Vault1Test.js
+++ test/Vault1Test.js
@@ -2,12 +2,61 @@ const { expect } = require("chai");
 const { ethers } = require("hardhat");
 
 describe("Vault 1", () => {
+
+	let Vault1;
+	let vault1Token;
+	let bottleCapsFactory;
+	let bottleCapsToken;
+	let addr1;
+	let addr2;
+
 	beforeEach(async () => {
-		Vault1 = await ethers.getContractFactory("Vault1");
-		[owner] = await ethers.getSigners();
+		[owner, addr1, addr2] = await ethers.getSigners();
 
-		vault1 = await Vault1.deploy();
+		bottleCapsFactory = await ethers.getContractFactory("BottleCaps", owner);
+		bottleCapsToken = await bottleCapsFactory.deploy()
+		await bottleCapsToken.deployed();
 
-		await vault1.deployed();
+		Vault1 = await ethers.getContractFactory("Vault1", owner);
+		vault1Token = await Vault1.deploy();
+
+		await vault1Token.deployed();
 	});
+
+
+	describe("Deployment", async function () {
+		it("Should assign the total supply of tokens to the owner", async function () {
+			const ownerBalance = await bottleCapsToken.balanceOf(owner.address);
+			expect(await bottleCapsToken.totalSupply()).to.equal(ownerBalance);
+		});
+	});
+
+	describe("#deposit", async function() {
+		it("should take in a deposit amount", async function() {
+			await bottleCapsToken.transfer(addr1.address, 3); // Have some caps to transfer
+			
+			let originalAddr1Balance = await bottleCapsToken.balanceOf(addr1.address);
+
+			await vault1Token.connect(addr1).deposit({
+				value: ethers.utils.parseEther("1.0")
+			});
+
+			expect(await bottleCapsToken.balanceOf(addr1.address)).to.eq(originalAddr1Balance - 3);
+			expect(await vault1Token.balanceOf(addr1.address)).to.eq(3);
+		})
+	});
+
+	// describe("#withdraw", async function() {
+	// 	it("should allow users to widthdraw <= what's deposited", async function() {
+	// 		await vault1Token.transfer(addr1.address, 50);
+			
+	// 		let originalOwnerBalance = await vault1Token.balanceOf(owner.address);
+	// 		let originalAddr1Balance = await vault1Token.balanceOf(addr1.address);
+
+	// 		await vault1Token.connect(addr1).withdraw(1);
+
+	// 		expect(await vault1Token.balanceOf(addr1.address)).to.eq(originalAddr1Balance + 1);
+	// 		expect(await vault1Token.balanceOf(owner.address)).to.eq(originalOwnerBalance - 1);
+	// 	})
+	// });
 });
