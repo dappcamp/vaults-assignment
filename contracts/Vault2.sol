a038bc9d2292132c32703f606f43cc947e6ad903
HARSHAL JETHWA <harshalj92@gmail.com>
2022-01-22 22:44:06 -0500

    Implemented Vault 2

--- contracts/Vault2.sol
+++ contracts/Vault2.sol
@@ -1,6 +1,20 @@
 //SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.4;
 
-contract Vault2 {
-    
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract Vault2 is ERC20 {
+    constructor() ERC20("Vault2", "VAULT") {}
+
+    function mint(uint _amount) external payable {
+      require(msg.value == _amount, "Ether sent should equal input amount");
+      _mint(msg.sender, msg.value);
+    }
+
+    function burn(uint _amount) external {
+      require(balanceOf(msg.sender) >= _amount, "Not enough to burn");
+      _burn(msg.sender, _amount);
+      (bool success, ) = msg.sender.call { value : _amount }("");
+      require(success, "failed to transfer via call");
+    }
 }
--- test/Vault2Test.js
+++ test/Vault2Test.js
@@ -4,10 +4,37 @@ const { ethers } = require("hardhat");
 describe("Vault 2", () => {
 	beforeEach(async () => {
 		Vault2 = await ethers.getContractFactory("Vault2");
-		[owner] = await ethers.getSigners();
+		const accounts = await ethers.getSigners();
+
+		owner = accounts[0];
+		account1 = accounts[1];
 
 		vault2 = await Vault2.deploy();
 
 		await vault2.deployed();
+	});
+
+	describe("mint", function () {
+		it("minting 100 should work", async function () {
+			await vault2.connect(account1).mint(100, {
+				value: 100
+			});
+			const account1Balance = await vault2.balanceOf(account1.address);
+			expect(account1Balance).to.equal(100);
+		});
+	});
+	
+	describe("burn", function () {
+		it("burning 100 should work", async function () {
+			await vault2.connect(account1).mint(100, {
+				value: 100
+			});
+			await vault2.connect(account1).burn(100);
+			const account1Balance = await vault2.balanceOf(account1.address);
+			expect(account1Balance).to.equal(0);
+		});
+		it("burning 200 should fail", async function () {
+			await expect(vault2.connect(account1).burn(200)).to.be.revertedWith("Not enough to burn");
+		});
 	});
 });
