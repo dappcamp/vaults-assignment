1566983b905365797d8f387a9742016b758042b9
Ted Wu <ted.wu@airbnb.com>
2022-01-22 17:43:17 -0800

    Vault2

--- contracts/Vault2.sol
+++ contracts/Vault2.sol
@@ -1,6 +1,29 @@
 //SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.4;
 
-contract Vault2 {
-    
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+
+contract Vault2 is ERC20, Ownable {
+    // Events
+    event Mint(uint amount);
+    event Burn(uint amount);
+
+
+    constructor() ERC20("Vault2", "VAULT") {}
+
+    function mint(uint _amount) external payable onlyOwner {
+        require(_amount == msg.value, "amount does not equal to msg.value");
+        _mint(msg.sender, _amount);
+        emit Mint(_amount);
+    }
+
+    function burn(uint _amount) external payable onlyOwner {
+        _burn(msg.sender, _amount);
+        (bool success,) = msg.sender.call{value: _amount}("");
+        require(success, "Withdraw failed");
+        emit Burn(_amount);
+    }
+
 }
--- test/Vault2Test.js
+++ test/Vault2Test.js
@@ -4,10 +4,52 @@ const { ethers } = require("hardhat");
 describe("Vault 2", () => {
 	beforeEach(async () => {
 		Vault2 = await ethers.getContractFactory("Vault2");
-		[owner] = await ethers.getSigners();
+		[owner, account1] = await ethers.getSigners();
 
 		vault2 = await Vault2.deploy();
 
 		await vault2.deployed();
+	});
+
+	describe("mint", () => {
+		it("should revert if not owner", async function() {
+			await expect(
+				vault2.connect(account1).mint(50, { value: 50 }))
+				.to.be.revertedWith("Ownable: caller is not the owner");
+		});
+
+		it("should emit Mint event when successful", async function() {
+			await expect(vault2.connect(owner).mint(50, { value: 50 }))
+				.to.emit(vault2, "Mint")
+				.withArgs(50);
+		});
+
+		it("should revert if msg.value is different", async function() {
+			await expect(
+				vault2.connect(owner).mint(50, { value: 100 }))
+				.to.be.revertedWith("amount does not equal to msg.value");
+		});
+	});
+
+	describe("burn", () => {
+		it("should revert if not owner", async function() {
+			await expect(
+				vault2.connect(account1).burn(50, { value: 50 }))
+				.to.be.revertedWith("Ownable: caller is not the owner");
+		});
+
+		it("should emit Burn event when successful", async function() {
+			await expect(vault2.connect(owner).mint(50, { value: 50 }));
+
+			await expect(vault2.connect(owner).burn(50, { value: 50 }))
+				.to.emit(vault2, "Burn")
+				.withArgs(50);
+		});
+
+		it("allow burning up to deposited amount", async function() {
+			await expect(vault2.connect(owner).mint(50, { value: 50 }));
+			await expect(vault2.connect(owner).burn(100))
+				.to.be.revertedWith("ERC20: burn amount exceeds balance");
+		});
 	});
 });
